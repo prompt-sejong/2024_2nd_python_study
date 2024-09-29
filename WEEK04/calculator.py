31525ab13dcd13ca338af02797ace0a8d366468f
tabsanro <dojan789@gmail.com>
2024-09-30 00:59:10 +0900

    feat: WEEK03 WEEK04 assignment

---
+++ WEEK04/calculator.py
@@ -0,0 +1,43 @@
+'''
+TODO: 
+1. 각 함수명에 걸맞는 연산을 수행하는 함수를 완성하세요.
+2. WEEK04.ipynb 에서 import calculator로 함수를 불러와 검증하는 부분을 추가하세요.
+3. 모듈 함수 사용방법은 calculator.sum_numbers(1, 2, 3) 식으로 사용할 수 있습니다.
+'''
+
+
+def sum_numbers(*args: int) -> int:
+    # 복수 개의 정수 입력을 받아 모두 더한 값을 출력하는 함수
+    # for 문을 사용하세요
+    pass
+
+
+def multiply(*args: int) -> int:
+    # 복수 개의 정수 입력을 받아 모두 곱한 값을 출력하는 함수
+    # for 문을 사용하세요
+    pass
+
+
+def factorial(n: int) -> int:
+    # n이 0 이상일 때, n! 을 반환하는 함수
+    # while 문을 사용하세요
+    pass
+
+
+def fibonacci(n: int) -> int:
+    # n 번째 피보나치 수를 반환하는 함수
+    # 조건문과 재귀함수를 이용하세요
+    pass
+
+
+def primes_up_to_n(n: int) -> int:
+    # 1부터 N까지의 정수 중에 소수를 모두 찾아 리스트로 반환하는 함수
+    # 리스트 컴프리헨션과 조건문을 이용하세요
+    pass
+
+
+def even_numbers(numbers: list[int]) -> list[int]:
+    # 정수 리스트에서 짝수들만 골라 리스트로 반환하는 함수를 만드세요
+    # 람다 함수식을 사용하세요
+    # return list(filter( ####, #### ))
+    pass
