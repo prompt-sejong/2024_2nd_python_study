06f3b44799620fd82976b7b73f8074ac3f16519c
mksd <86861487+m02k07k25@users.noreply.github.com>
2024-11-22 23:53:12 +0900

    7주차 임시제출

---
+++ WEEK08/mksd/node.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import os
+
+db_path = os.path.join(os.path.dirname(__file__), '실시간도착_역정보(20241110).xlsx')
+
+# 데이터 불러오기
+df = pd.read_excel(db_path)
+s = df[df['STATN_NM'] == '서울']
+print(s)
+
+
+# 현재 위치 : X -> 도착 위치 : Y
